d1d0cc5a1e582d6142479b54670c3cf5933899dd
LUTHFIANI NUR AISYAH <222112162@stis.ac.id>
2024-08-22 03:56:52 +0700

    ganti

--- resources/views/supervisor/supervisor_master.blade.php
+++ resources/views/supervisor/supervisor_master.blade.php
@@ -1,201 +1,148 @@
 <!doctype html>
 <html lang="en">
 
-    <head>
-        
-        <meta charset="utf-8" />
-        <title>Dashboard | SIMUTAN - Supervisor Dashboard Template</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesdesign" name="author" />
+<head>
+    <meta charset="utf-8" />
+    <title>Dashboard Supervisor | SIMUTAN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
 
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.20.0/tabler-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
 
-        <!-- jquery.vectormap css -->
-        <link href="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 
-        <!-- DataTables -->
-        <link href="{{ asset('backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- DataTables CSS -->
+    <link href="{{ asset('backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+    {{-- <link rel="stylesheet" href="{{ asset('css/buttons.dataTables.min.css') }}"> --}}
 
-        <!-- Responsive datatable examples -->
-        <link href="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />  
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('backend/assets/css/bootstrap.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons CSS -->
+    <link href="{{ asset('backend/assets/css/icons.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App CSS -->
+    <link href="{{ asset('backend/assets/css/app.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
-        <!-- Bootstrap Css -->
-        <link href="{{ asset('backend/assets/css/bootstrap.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="{{ asset('backend/assets/css/icons.css') }}" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="{{ asset('backend/assets/css/app.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+    <link rel="stylesheet" href="{{ asset('vendor/libs/bs-stepper/bs-stepper.css') }}" />
 
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
-        <link rel="stylesheet" href="{{ asset("vendor/libs/bs-stepper/bs-stepper.css") }}" />
+    <style>
+        .table-wrapper {
+            overflow-x: auto;
+        }
+        table.dataTable {
+            border-collapse: collapse !important;
+            width: 100%;
+        }
+    </style>
+</head>
 
-        <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+<body data-topbar="dark">
 
-  <title>Sirema - Ajukan Request</title>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
 
-  <meta name="description" content="" />
+        @include('supervisor.body.header')
 
-  <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="{{ asset("img/favicon/favicon.ico") }}" />
+        <!-- Left Sidebar -->
+        @include('supervisor.body.sidebar')
+        <!-- End Left Sidebar -->
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-
-  <!-- Icons -->
-  <link rel="stylesheet" href="{{ asset("vendor/fonts/fontawesome.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/fonts/tabler-icons.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/fonts/flag-icons.css") }}" />
-
-  <!-- Core CSS -->
-  <link rel="stylesheet" href="{{ asset("vendor/css/rtl/core.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/css/rtl/theme-default.css") }}" />
-  <link rel="stylesheet" href="{{ asset("css/demo.css") }}" />
-
-  <!-- Vendors CSS -->
-  <!-- Vendors CSS -->
-  <link rel="stylesheet" href="{{ asset("vendor/libs/perfect-scrollbar/perfect-scrollbar.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/node-waves/node-waves.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/typeahead-js/typeahead.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/bs-stepper/bs-stepper.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/bootstrap-select/bootstrap-select.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/select2/select2.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/formvalidation/dist/css/formValidation.min.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/flatpickr/flatpickr.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/bootstrap-datepicker/bootstrap-datepicker.css") }}" />
-  <link rel="stylesheet" href="{{ asset("vendor/libs/pickr/pickr-themes.css") }}" />
-
-  <!-- Include SweetAlert2 CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
-
-  <!-- Page CSS -->
-
-  <!-- Helpers -->
-  <script src="{{ asset("vendor/js/helpers.js") }}"></script>
-
-  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-  <script src="{{ asset("js/config.js") }}"></script>
-
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <style>
-    .table-wrapper {
-        overflow-x: auto;
-    }
-    table.dataTable {
-        border-collapse: collapse !important;
-        width: 100%;
-    }
-</style>
-    </head>
-
-    <body data-topbar="dark">
-    
-    <!-- <body data-layout="horizontal" data-topbar="dark"> -->
-
-        <!-- Begin page -->
-        <div id="layout-wrapper">
-
-            
-          @include('supervisor.body.header')
-
-            <!-- ========== Left Sidebar Start ========== -->
-           @include('supervisor.body.sidebar')
-            <!-- Left Sidebar End -->
-
-            
-
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="main-content">
-
-               @yield('supervisor')
-                <!-- End Page-content -->
-
-                @include('supervisor.body.footer')
-                
-                
-            </div>
-            <!-- end main content-->
-
+        <!-- Main Content -->
+        <div class="main-content">
+            @yield('supervisor')
+            @include('supervisor.body.footer')
         </div>
-        <!-- END layout-wrapper -->
+        <!-- End Main Content -->
 
-        <!-- Right Sidebar -->
-        
-        <!-- /Right-bar -->
+    </div>
+    <!-- End layout-wrapper -->
 
-        <!-- Right bar overlay-->
-        <div class="rightbar-overlay"></div>
+    <!-- Right Sidebar -->
+    <!-- /Right-bar -->
 
-        <!-- JAVASCRIPT -->
-        <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
+    <!-- Right bar overlay -->
+    <div class="rightbar-overlay"></div>
 
-        
-        <!-- apexcharts -->
-        <script src="{{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
 
-        <!-- jquery.vectormap map -->
-        <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}"></script>
+    {{-- <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    
+    <!-- Responsive examples -->
+    <script src="{{ asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
 
-        <!-- Required datatable js -->
-        <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-        
-        <!-- Responsive examples -->
-        <script src="{{ asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+         <!-- Required datatable js -->
+         <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+         <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    
+         <!-- Datatable init js -->
+         <script src="{{ asset('backend/assets/js/pages/datatables.init.js') }}"></script> --}}
 
-        <script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
+    <!-- ApexCharts -->
+    <script src="{{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
-        <!-- App js -->
-        <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+    <!-- jQuery Vector Map -->
+    <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}"></script>
+
+    <!-- DataTables JS -->
+    <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+
+    <!-- DataTables Buttons JS -->
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+
+    <script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+
+    <!-- Handle Bars JS -->
+    <script src="{{ asset('backend/assets/js/handlebars.js') }}"></script>
 
 
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-<script>
- @if(Session::has('message'))
- var type = "{{ Session::get('alert-type','info') }}"
- switch(type){
-    case 'info':
-    toastr.info(" {{ Session::get('message') }} ");
-    break;
+    <!-- Toastr JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type', 'info') }}";
+        switch(type){
+            case 'info':
+                toastr.info("{{ Session::get('message') }}");
+                break;
+            case 'success':
+                toastr.success("{{ Session::get('message') }}");
+                break;
+            case 'warning':
+                toastr.warning("{{ Session::get('message') }}");
+                break;
+            case 'error':
+                toastr.error("{{ Session::get('message') }}");
+                break; 
+        }
+        @endif
+    </script>
 
-    case 'success':
-    toastr.success(" {{ Session::get('message') }} ");
-    break;
+    <!-- Notify JS (Optional) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/notify.js/2.0.0/notify.min.js" integrity="sha512-iy8/ErLJUuqWbu30yUSCxXtE3FCDZi3y5op0Duqdp7vtpeh1E6ZyAPnRS+OrJHddh4uP30oYpwNt7TXPbmP5lQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    case 'warning':
-    toastr.warning(" {{ Session::get('message') }} ");
-    break;
-
-    case 'error':
-    toastr.error(" {{ Session::get('message') }} ");
-    break; 
- }
- @endif 
-</script>
-
-        <!-- Required datatable js -->
-        <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-
-        <!-- Datatable init js -->
-        <script src="{{ asset('backend/assets/js/pages/datatables.init.js') }}"></script>
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/notify.js/2.0.0/notify.min.js" integrity="sha512-iy8/ErLJUuqWbu30yUSCxXtE3FCDZi3y5op0Duqdp7vtpeh1E6ZyAPnRS+OrJHddh4uP30oYpwNt7TXPbmP5lQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    </body>
+</body>
 
 </html>
